aefc462e392e5ad99115ca2322836f99d0dcc7f6
s-terauchi <shunsuke091228@gmail.com>
2018-02-20 21:14:30 +0900

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
